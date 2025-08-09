73e56bde179a77315568721e386021dd0d6c784c
swapnilpalandure9 <swapnil.palandure9@gmail.com>
2025-08-09 15:52:24 +0530

    commit changes

---
+++ WebApi/Encrypt&Decrypt/StringEncryptionService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WebApi.Encrypt_Decrypt
+{
+public static class StringEncryptionService
+{
+    private static readonly string key = "your-32-char-secret-key-here"; // 256-bit key
+    private static readonly string iv = "your-16-char-iv-here"; // 128-bit IV
+
+    public static string Encrypt(string plainText)
+    {
+        using var aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(key);
+        aes.IV = Encoding.UTF8.GetBytes(iv);
+
+        using var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+        using var ms = new MemoryStream();
+        using var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
+        using var sw = new StreamWriter(cs);
+
+        sw.Write(plainText);
+        sw.Close();
+
+        return Convert.ToBase64String(ms.ToArray());
+    }
+
+    public static string Decrypt(string cipherText)
+    {
+        using var aes = Aes.Create();
+        aes.Key = Encoding.UTF8.GetBytes(key);
+        aes.IV = Encoding.UTF8.GetBytes(iv);
+
+        using var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+        using var ms = new MemoryStream(Convert.FromBase64String(cipherText));
+        using var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+        using var sr = new StreamReader(cs);
+
+        return sr.ReadToEnd();
+    }
+}
+    }
